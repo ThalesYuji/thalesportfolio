5bb21ec73983995c2ada2eac0c9e41de22c3e520
ThalesYuji <thalesyuuji@gmail.com>
2025-12-25 00:07:47 -0300

    Initial commit - Portfolio Thales

--- app/fonts.ts
+++ app/fonts.ts
@@ -2,7 +2,7 @@
 import localFont from "next/font/local";
 
 export const mamboe = localFont({
-  src: "./fonts/MamboeTrial.otf",
+  src: "./fonts/AKONY.otf",
   display: "swap",
   variable: "--font-mamboe",
 });
